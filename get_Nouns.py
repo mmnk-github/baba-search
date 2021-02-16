21d4a893bcb846e97f8a421603f56310c681ece0
mmnk-github <48mimineko@gmail.com>
2021-02-17 00:03:13 +0900

    jsonファイルに書き込んだ

--- get_Nouns.py
+++ get_Nouns.py
@@ -19,21 +19,29 @@ table = soup.select_one(".table")
 for a in table.select("tr"):
     img = a.select_one("img")
     if img != None:
-        print(img)
         name = img["alt"][5:-6]
         gif = img["data-src"]
         data.append({"name": name, "gif": gif})
 
-{
-    "FOFO": {
-        "sorted_name": "OOFF",
-        "img": "FOFO.gif"
-    },
-    
-}
+aisu = {}
 
-# for d in data:
-#     r = requests.get(d["gif"])
-#     print(r)
-#     with open("./Pages/image/" + d["name"] + ".gif", "wb") as file:
-#         file.write(r.content)
+for d in data:
+    aisu[d['name']] = {"sorted_name": sorted(d['name']), "img": "./Pages/image/" + d["name"] + ".gif"}
+    # r = requests.get(d["gif"])
+    # print(r)
+    # with open("./Pages/image/" + d["name"] + ".gif", "wb") as file:
+    #     file.write(r.content)
+
+sym = open("obj_name_data.json", "w")
+json.dump(aisu, sym)
+
+# {
+#     "FOFO": {
+#         "sorted_name": "OOFF",
+#         "img": "FOFO.gif"
+#     },
+#     "BABA": {
+#         "sorted_name": "AABB",
+#         "img": "BABA.gif"
+#     }
+# }
